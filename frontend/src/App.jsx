a65d6c2c4dda51287d8a2d4808729e76fd813a00
avi212003 <chauhanavi212003@gmail.com>
2025-12-26 01:06:37 -0500

    connection of backend and frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,15 +9,26 @@ import ChatPage from "./pages/ChatPage.jsx";
 import OnboardingPage from "./pages/OnboardingPage.jsx";
 
 import { Toaster } from "react-hot-toast";
-
+import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import axiosInstance from "./lib/axios.js";
 
 const App = () => {
   
+  const { data:authData, isLoading, error } = useQuery({
+    queryKey: ["authUser"],
+    queryFn: async () => {
+      const res = await axiosInstance.get("/auth/me");
+      return res.data;
+    },
+  });
+  console.log(data); 
+  const authUser = authData?.user; 
 
   return (
-    <div className="h-screen" data-theme={theme}>
+    <div className="h-screen" data-theme="night">
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
         <Route path="/signup" element={<SignUpPage />} /> 
         <Route path="/login" element={<LoginPage />} />
         <Route path="/notifications" element={<NotificationsPage />} />
